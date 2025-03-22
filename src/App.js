2934ea5402227b533c0dd538e655d8b37014af08
Arold <aroldtsatio@gmail.com>
2025-03-22 12:46:34 +0100

    Choose Language

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import ChooseLanguage from './components/ChooseLanguage';
 
 function App() {
+  const [language, setLanguage] = React.useState("en");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+       {/* Choose Language */}
+       <ChooseLanguage language={language} setLanguage={setLanguage} />
     </div>
   );
 }
---
+++ src/components/ChooseLanguage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function ChooseLanguage({ language, setLanguage}) {
+    return (
+        <div className="mb-4"> 
+           <label className="mr-2">Choose your language:</label>
+              <select value={language} onChange={(e) => setLanguage(e.target.value)}>
+                <option value="en">English</option>
+                <option value="fr">French</option>
+                <option value="de">German</option> 
+              </select>     
+        </div>
+    );
+    }
